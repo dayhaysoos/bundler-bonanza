75874053d3ae0779e343bc89d280b8eff487e811
Nick DeJesus <ndejesus1227@gmail.com>
2023-07-19 14:09:52 -0400

    wild jest things

---
+++ jest/jest-pnpm/create-did.test.js
@@ -0,0 +1,22 @@
+import { Web5 } from "@tbd54566975/web5";
+
+let did;
+
+beforeEach(async () => {
+  const result = await createDid();
+  did = result.did;
+});
+
+async function createDid() {
+  const { web5, did } = await Web5.createDid();
+  return { web5, did };
+}
+
+test("create a did", async () => {
+  const { did } = await createDid();
+  expect(did).toBeDefined();
+});
+
+test("adds 1+1 to equal 2", () => {
+  expect(1 + 1).toBe(2);
+});
